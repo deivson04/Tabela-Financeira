6c8c22f253e60e42de22dbc13a027de9255d0767
deivson <dayvson04@hotmail.com>
2023-02-03 21:54:32 -0300

    Alteração no projeto

--- resources/views/cadastrar_usuario.blade.php
+++ resources/views/cadastrar_usuario.blade.php
@@ -1,1 +1,2 @@
+ @extends('layout.app')
 <a href="/">Ja tenho cadastro<a
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="pt">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">   
+    <title>Tabela Financeira</title>
+    <!-- Bootstrap CSS -->
+    <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('css/tabelaFinanceira.css')}}" rel="stylesheet">
+</head>
+<body>
+  <h1 class="title">Tabela Financeira</h1>      
+        <div class="container">
+          
+        @yield('conteudo')
+        
+        
+        </div>   
+            
+    
+  
+  <script src="js/bootstrap.bundle.min.js"></script> 
+  <script src="{{asset('js/tabelaFinanceira.js')}}"></script> 
+
+</body>
+</html>
+
